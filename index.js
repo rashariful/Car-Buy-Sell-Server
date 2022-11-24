c81dbb6b967669d93a4fb209c71c6b391d487d38
md shariful islam <raislam.shariful@gmail.com>
2022-11-24 15:48:46 +0600

    product post and get function created

--- index.js
+++ index.js
@@ -1,19 +1,75 @@
 const express = require('express');
-const app = express()
+const { MongoClient, } = require('mongodb');
+const app = express();
+require("dotenv").config()
 const cors = require('cors');
 const port = process.env.PORT || 5000
 
 
-// middle ware
+// Middle ware
 app.use(cors())
-app.use(express())
+app.use(express.json())
+
+
+
+
+
+
+
+
+const uri = `mongodb://localhost:27017`;
+const client = new MongoClient(uri)
+
+const productsCollection = client.db('doctorsPortal').collection('products')
+
+async function dbConnect() {
+    try {
+        await client.connect()
+        console.log('mongodb connected with server');
+    } catch (error) {
+        console.log(error);
+    }
+}
+dbConnect()
+
+
+app.post('/products', async (req, res) =>{
+   try {
+       const product = req.body;
+       const result = await productsCollection.insertOne(product)
+       res.send(result)
+   } catch (error) {
+    console.log(error.message);
+    
+   }
+})
+
+app.get('/products', async ( req, res) =>{
+    try {
+        const query = {}
+        const result = await productsCollection.find(query).toArray()
+        res.send(result)
+    } 
+    catch (error) {
+        console.log(error.message);
+    }
+})
+
+
+
+
+
+
+
+
+
 
 
 
 
 app.get('/', (req, res) => {
-    res.send('Used car server is running')
+    res.send('Doctor portal server is running')
 })
 app.listen(port, () => {
-    console.log(`Used car  server is running on: ${port}`);
+    console.log(`Doctor portal server is running on: ${port}`);
 })
